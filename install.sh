f8cacf70abdc2fbc85228ffe4f3f2ae16aacc5f3
gajubhat <gajukbhat@gmail.com>
2013-12-05 22:32:44 +0530

    Using install dir variable for cleaner code

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/local/bin/bash
+install_dir="$HOME/.dot-file-collection"
 # Recognize dot files correctly
 shopt -s dotglob
 shopt -s nullglob
 
 files=(./sym_links/*)
 for file in sym_links/* ; do
-        ln -sf "$HOME/.dot-file-collection/${file}" $HOME/ ;
+        ln -sf "$install_dir/${file}" $HOME/ ;
 done
